bfd4497609388aa673d03233ebbffa54dfd8a035
tashaffin <jcodystockwell@gmail.com>
2022-01-22 09:38:04 -0700

    first commit

---
+++ src/lambda.js
@@ -0,0 +1,14 @@
+export async function handler(event) {
+  return {
+    statusCode: 200,
+    headers: { "Content-Type": "text/html" },
+    body: `Hello, World! Your request was received at ${event.requestContext.time}.\n` +
+    displayProperties(event)
+  };
+}
+
+function displayProperties(o) {
+  return '<ul>' +
+  (typeof(o) != 'object' ? o : Object.getOwnPropertyNames(o).map(p => '<li>' + p + displayProperties(o[p]) + '</li>').join('')) +
+  '</ul>';
+}
