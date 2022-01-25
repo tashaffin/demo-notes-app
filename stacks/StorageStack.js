0bb600a28d2921b057565eafedf39dd04d14cd39
vajdaij <vajdaij@gmail.com>
2022-01-24 19:17:51 -0700

    updating StorageStack.js per tutorial pg 66

--- stacks/StorageStack.js
+++ stacks/StorageStack.js
@@ -1,11 +1,14 @@
 import * as sst from "@serverless-stack/resources";
 
 export default class StorageStack extends sst.Stack {
+    // Public reference to the bucket
+    bucket;
     // Public reference to the table
     table;
     constructor(scope, id, props) {
         super(scope, id, props);
         // Create the DynamoDB table
+        this.bucket = new sst.Bucket(this, "Uploads");
         this.table = new sst.Table(this, "Notes", {
             fields: {
                 userId: sst.TableFieldType.STRING,
