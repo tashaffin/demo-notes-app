94660b83b5d13be4bde79c23fb8e431401ca01bf
tashaffin <jcodystockwell@gmail.com>
2022-01-24 18:49:16 -0700

    Fix indentation

--- stacks/StorageStack.js
+++ stacks/StorageStack.js
@@ -1,16 +1,15 @@
 export default class StorageStack extends sst.Stack {
-// Public reference to the table
-table;
-constructor(scope, id, props) {
-super(scope, id, props);
-// Create the DynamoDB table
-this.table = new sst.Table(this, "Notes", {
-fields: {
-userId: sst.TableFieldType.STRING,
-noteId: sst.TableFieldType.STRING,
-},
-primaryIndex: { partitionKey: "userId", sortKey: "noteId" },
-});
+    // Public reference to the table
+    table;
+    constructor(scope, id, props) {
+        super(scope, id, props);
+        // Create the DynamoDB table
+        this.table = new sst.Table(this, "Notes", {
+            fields: {
+                userId: sst.TableFieldType.STRING,
+                noteId: sst.TableFieldType.STRING,
+            },
+            primaryIndex: { partitionKey: "userId", sortKey: "noteId" },
+        });
+    }
 }
-}
-
