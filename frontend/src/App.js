f62fc9b38bffd2caf4b8c96672fc0b642d141bca
tashaffin <jcodystockwell@gmail.com>
2022-02-19 08:48:19 -0700

    Added signup and login menu items

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,26 @@ import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import "./App.css";
 import Routes from "./Routes";
+import Nav from "react-bootstrap/Nav";
 
 function App() {
   return (
     <div className="App container py-3">
       <Navbar collapseOnSelect bg="light" expand="md" className="mb-3">
-        <Navbar.Brand className="font-weight-bold text-muted">
+        <Navbar.Brand href="/" className="font-weight-bold text-muted">
           Scratch
         </Navbar.Brand>
         <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Nav>
+            <Nav.Link href="/signup">Signup</Nav.Link>
+            <Nav.Link href="/login">Login</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
       <Routes />
     </div>
   );
 }
+
 export default App;
