cfd379790b142bdc43f94e1df7c5baabc376bbf6
tashaffin <jcodystockwell@gmail.com>
2022-03-05 08:35:13 -0700

    Signup page with custom hook

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -3,6 +3,7 @@ import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import NotFound from "./containers/NotFound";
 import Login from "./containers/Login";
+import Signup from "./containers/Signup";
 import Satan from "./containers/Satan";
 
 export default function Routes() {
@@ -13,6 +14,9 @@ export default function Routes() {
 			</Route>
 			<Route exact path="/login">
 				<Login />
+			</Route>
+			<Route exact path="/signup">
+				<Signup />
 			</Route>
 			<Route exact path="/satan">
 				<Satan />
