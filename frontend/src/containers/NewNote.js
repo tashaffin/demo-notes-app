9ac495eab6e3fcba89ee9ffc58b5c82eb592ba5d
tashaffin <jcodystockwell@gmail.com>
2022-03-07 18:33:50 -0700

    Attachments

--- frontend/src/containers/NewNote.js
+++ frontend/src/containers/NewNote.js
@@ -5,6 +5,8 @@ import LoaderButton from "../components/LoaderButton";
 import { onError } from "../lib/errorLib";
 import config from "../config";
 import "./NewNote.css";
+import { API } from "aws-amplify";
+import { s3Upload } from "../lib/awsLib";
 
 export default function NewNote() {
 	const file = useRef(null);
@@ -14,20 +16,37 @@ export default function NewNote() {
 	function validateForm() {
 		return content.length > 0;
 	}
+
 	function handleFileChange(event) {
 		file.current = event.target.files[0];
 	}
+
 	async function handleSubmit(event) {
 		event.preventDefault();
 		if (file.current && file.current.size > config.MAX_ATTACHMENT_SIZE) {
 			alert(
-				`Please pick a file smaller than ${config.MAX_ATTACHMENT_SIZE /
-				1000000} MB.`
+				`Please pick a file smaller than ${config.MAX_ATTACHMENT_SIZE / 1000000
+				} MB.`
 			);
 			return;
 		}
 		setIsLoading(true);
+		try {
+			const attachment = file.current ? await s3Upload(file.current) : null;
+			await createNote({ content, attachment });
+			history.push("/");
+		} catch (e) {
+			onError(e);
+			setIsLoading(false);
+		}
 	}
+
+	function createNote(note) {
+		return API.post("notes", "/notes", {
+			body: note
+		});
+	}
+
 	return (
 		<div className="NewNote">
 			<Form onSubmit={handleSubmit}>
