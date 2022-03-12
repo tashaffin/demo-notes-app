81e236dec65d6be07eae143c5a8bc51abeb40f4a
vajdaij <vajdaij@gmail.com>
2022-03-12 08:51:36 -0700

    updating Settings.js - pg 262

--- frontend/src/containers/Settings.js
+++ frontend/src/containers/Settings.js
@@ -4,6 +4,9 @@ import { useHistory } from "react-router-dom";
 import { onError } from "../lib/errorLib";
 import config from "../config";
 import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import BillingForm from "../components/BillingForm";
+import "./Settings.css";
 
 export default function Settings() {
 	const history = useHistory();
@@ -14,8 +17,37 @@ export default function Settings() {
 			body: details
 		});
 	}
+	async function handleFormSubmit(storage, { token, error }) {
+		if (error) {
+			onError(error);
+			return;
+		}
+		setIsLoading(true);
+		try {
+			await billUser({
+				storage,
+				source: token.id,
+			});
+			alert("Your card has been charged successfully!");
+			history.push("/");
+		} catch (e) {
+			onError(e);
+			setIsLoading(false);
+		}
+	}
 	return (
 		<div className="Settings">
-		</div>
+			<Elements
+				stripe={stripePromise}
+				fonts={[
+					{
+						cssSrc:
+							"https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800",
+					},
+				]}
+			>
+				<BillingForm isLoading={isLoading} onSubmit={handleFormSubmit} />
+			</Elements>
+		</div >
 	);
 }
