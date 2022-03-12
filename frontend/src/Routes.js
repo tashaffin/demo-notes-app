e865d393d5d3262271cb6b7d5685edd3094bdbc1
vajdaij <vajdaij@gmail.com>
2022-03-12 09:00:32 -0700

    readding Settings import to Routes.js

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -7,6 +7,7 @@ import Signup from "./containers/Signup";
 import NewNote from "./containers/NewNote";
 import Notes from "./containers/Notes";
 import Satan from "./containers/Satan";
+import Settings from "./containers/Settings";
 
 export default function Routes() {
 	return (
