eba66412d65ef308f844e7efe37a81e8bc210da1
tashaffin <jcodystockwell@gmail.com>
2022-03-28 19:02:06 -0600

    Initial Sentry setup

--- frontend/src/lib/errorLib.js
+++ frontend/src/lib/errorLib.js
@@ -1,3 +1,23 @@
+import * as Sentry from "@sentry/browser";
+import config from "../config";
+
+const isLocal = process.env.NODE_ENV === "development";
+export function initSentry() {
+	if (isLocal) {
+		return;
+	}
+	Sentry.init({ dsn: config.SENTRY_DSN });
+}
+export function logError(error, errorInfo = null) {
+	if (isLocal) {
+		return;
+	}
+	Sentry.withScope((scope) => {
+		errorInfo && scope.setExtras(errorInfo);
+		Sentry.captureException(error);
+	});
+}
+
 export function onError(error) {
 	let message = error.toString();
 	// Auth errors
